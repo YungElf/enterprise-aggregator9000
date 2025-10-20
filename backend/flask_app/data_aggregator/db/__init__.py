40c53af9579b6f7b963caf5fc97ada6c39fa2a9a
NixElf <agroxelf@gmail.com>
2025-10-20 01:56:06 -0700

    trying to fix db issue

--- backend/flask_app/data_aggregator/db/__init__.py
+++ backend/flask_app/data_aggregator/db/__init__.py
@@ -1,7 +1,9 @@
-from .db import get_conn, upsert_apigee_config_data, upsert_apigee_metrics
+from .db import get_conn, upsert_apigee_config_data, upsert_apigee_metrics, upsert_enterprise_api_apigee_metadata, upsert_enterprise_api_volume_metrics
  
 __all__ = [
     "get_conn",
     "upsert_apigee_config_data",
     "upsert_apigee_metrics",
+    "upsert_enterprise_api_apigee_metadata",
+    "upsert_enterprise_api_volume_metrics",
 ] 
